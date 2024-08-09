cfabbfe324eec718d6fd700bc66e83b4ec924545
toastoffee <931391442@qq.com>
2024-08-09 12:12:48 +0800

    add cartridge class

---
+++ src/cartridge.h
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : cartridge.h
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/8/9
+  ******************************************************************************
+  */
+
+
+
+#ifndef GAMEBOY_EMULATOR_CARTRIDGE_H
+#define GAMEBOY_EMULATOR_CARTRIDGE_H
+
+
+class cartridge {
+
+};
+
+
+#endif //GAMEBOY_EMULATOR_CARTRIDGE_H
