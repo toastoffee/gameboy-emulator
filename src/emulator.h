37ff858db4154f1715c0ff0ae6bdf2bf8d9b9a67
toastoffee <931391442@qq.com>
2024-08-10 11:43:48 +0800

    add emulator class

---
+++ src/emulator.h
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : emulator.h
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/8/10
+  ******************************************************************************
+  */
+
+
+
+#ifndef GAMEBOY_EMULATOR_EMULATOR_H
+#define GAMEBOY_EMULATOR_EMULATOR_H
+
+
+class emulator {
+
+};
+
+
+#endif //GAMEBOY_EMULATOR_EMULATOR_H
