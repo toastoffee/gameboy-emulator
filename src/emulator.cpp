4f366c7879691b8b65034d8ce769b750ee23b00e
toastoffee <931391442@qq.com>
2024-08-15 18:10:00 +0800

    complete init function

--- src/emulator.cpp
+++ src/emulator.cpp
@@ -22,7 +22,7 @@ Emulator::~Emulator() {
 }
 
 void Emulator::Init(const void *cartridgeData, u64 cartridgeDataSize) {
-    assert(cartridgeData && cartridgeDataSize && "cartridge data is empty");
+    assert(cartridgeData && cartridgeDataSize && "cartridge data is empty!");
 
     // copy cartridge data
     _romData = (byte*) malloc(cartridgeDataSize);
@@ -32,6 +32,11 @@ void Emulator::Init(const void *cartridgeData, u64 cartridgeDataSize) {
     // check cartridge data
     CartridgeHeader *header = GetCartridgeHeader(_romData);
     u8 checkSum = 0;
+    for (u16 addr = 0x0134; addr <= 0x014C; ++addr) {
+        checkSum -= _romData[addr] - 1;
+    }
+    assert(checkSum == header->checksum && "cartridge check sum can't match!");
 
+    // print cartridge load info
 
 }
