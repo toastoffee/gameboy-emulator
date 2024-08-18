210a9bb64dbb880f27406c4ae3cedeacc86a7645
toastoffee <931391442@qq.com>
2024-08-18 11:00:17 +0800

    add debug window class

---
+++ src/debug_window.h
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  * @file           : debug_window.h
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/8/18
+  ******************************************************************************
+  */
+
+
+
+#ifndef GAMEBOY_EMULATOR_DEBUG_WINDOW_H
+#define GAMEBOY_EMULATOR_DEBUG_WINDOW_H
+
+#include <imgui/imgui.h>
+
+
+class DebugWindow {
+
+
+};
+
+
+#endif //GAMEBOY_EMULATOR_DEBUG_WINDOW_H
